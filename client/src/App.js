a9d393d05f7afa72f98187bdb4e3c143394c7e55
SlayerT26 <456theochang@gmail.com>
2021-10-06 12:56:13 -0700

    homepage done

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+
+import { Route } from 'react-router';
+import './App.css';
+import Homepage from './screens/Homepage/Homepage';
+import Login from './screens/Login/Login'
+import CreateAccount from './screens/CreateAccount/CreateAccount'
+
+
+function App() {
+  return (
+    <>
+      <Homepage />
+      <Route path='/login' exact>
+        <Login />
+      </Route>
+      <Route path='/createaccount' exact>
+        <CreateAccount />
+      </Route>
+
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/screens/Homepage/Homepage.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom"
+import './Homepage.css'
+
+function Homepage() {
+  return (
+    <>
+      <div className="HomeFlex1">
+        <div className="Home-nav">
+          <div>
+            <img src="https://i.imgur.com/3l4jp3Y.png" alt="ransom" width="700px" />
+          </div>
+          <div className="div-login">
+            <Link to='/login' id="HomeLogin">
+              <h2 className="HomeLogin">Login</h2>
+            </Link>
+          </div>
+        </div>
+        <div className="HomeParagraph">
+          <p />
+
+          <p className="Para1">Welcome to Time2Riddle. Do you enjoy being challenged?<br />
+            Being creative? Or both?<br />
+            Then this is your app.<br />
+            Time 2 Riddle will allow users to solve riddles. And if solving<br />
+            isn’t enough, then create them. Create the most elaborate riddle for other user’s.<br />
+            The app will also allow you to save your favorite riddle in your profile. <br />
+            So you can come back, to try and solve it again or send to a friend.<br />
+            <Link to='/createaccount' id="HomeToSignup">
+              <p className="Para2">Riddle me this and click here to start</p>
+            </Link>
+          </p>
+          <p />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Homepage
