fbeb1d7e1d5fef4e6ec26f8fd9dca3b02bdd7ec3
SlayerT26 <456theochang@gmail.com>
2021-10-11 07:00:09 -0700

    heroku url

--- client/src/services/config.js
+++ client/src/services/config.js
@@ -1,8 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseUrl = process.env.NODE_ENV === 'production'
-  ? 'https://time2riddle.herokuapp.com/' :
-  'http://localhost:3000';
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://time2riddle-api.herokuapp.com/"
+    : "http://localhost:3000";
 
 const api = axios.create({
   baseURL: baseUrl,
